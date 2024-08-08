41fee6e6709e25413aabeb723ccd2204c0c1c7f2
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-08-08 19:52:29 +0300

    FieldValidationError fix

--- backend/src/main/java/ru/flamexander/transfer/service/core/backend/errors/FieldValidationError.java
+++ backend/src/main/java/ru/flamexander/transfer/service/core/backend/errors/FieldValidationError.java
@@ -1,5 +1,10 @@
 package ru.flamexander.transfer.service.core.backend.errors;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class FieldValidationError {
     private String fieldName;
     private String message;
